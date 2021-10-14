1b278ddb33472eda129292c8d1601098671c5b09
scroobius-pip <sim04ful@gmail.com>
2021-10-14 19:50:23 +0100

    ui fixes

--- site/components/Section.tsx
+++ site/components/Section.tsx
@@ -1,4 +1,4 @@
-import { Stack, Typography } from '@mui/material'
+import { Stack, Typography, Box } from '@mui/material'
 import React from 'react'
 
 interface Props {
@@ -15,12 +15,15 @@ interface Props {
 const Section = ({ children, heading, subheading, headingStyle = { component: 'h1', variant: 'h5' } }: Props) => {
     // const { variant, component } = headingStyle
     return <Stack width='100%' height='100%' display='flex' justifyContent='center'>
-        <Typography width='100%' fontWeight='600' textAlign='left' component={headingStyle.component as any} variant={headingStyle.variant as any}>
-            {heading}
-        </Typography>
-        <Typography width='100%' marginBottom={4} textAlign='left'>
-            {subheading}
-        </Typography>
+        <Box marginBottom={4}>
+            <Typography width='100%' fontWeight='600' textAlign='left' component={headingStyle.component as any} variant={headingStyle.variant as any}>
+                {heading}
+            </Typography>
+            {subheading && <Typography width='100%' textAlign='left'>
+                {subheading}
+            </Typography>}
+
+        </Box>
         {children}
     </Stack>
 }
